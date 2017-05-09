655ad660d0c672f50d32666d22ee5e5ee0c746e9
kamalraj <kamalrajmyilsamy@gmail.com>
2017-05-09 13:06:28 +0530

    My first commit

--- Label/ViewController.swift
+++ Label/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        theLabel.text = "Welcome Home!"
+    }
+    @IBAction func buttonTapped2(_ sender: Any) {
+        theLabel.text = "Buttons are Cool!"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.cyan
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +33,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
